cd4bc72d454ec7b67e619c4afce90167fe66a0be
Ashok <askashokkumar.r@gmail.com>
2026-09-12 09:29:13 +0530

    Add --break-system-packages flag to pip install

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,8 @@
-#!/bin/bash
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput --clear
+# #!/bin/bash
+# pip install -r requirements.txt
+# python manage.py collectstatic --noinput --clear
+
+
+# Example build_files.sh
+python3.9 -m pip install -r requirements.txt --break-system-packages
+python3.9 manage.py collectstatic --noinput
