e1d5e51e0f88c19a4d32aa96af0e1d5fe9613888
Ashok <askashokkumar.r@gmail.com>
2026-09-12 09:32:09 +0530

    Fix pip install externally-managed-environment error

--- build_files.sh
+++ build_files.sh
@@ -3,6 +3,5 @@
 # python manage.py collectstatic --noinput --clear
 
 
-# Example build_files.sh
 python3.9 -m pip install -r requirements.txt --break-system-packages
 python3.9 manage.py collectstatic --noinput
